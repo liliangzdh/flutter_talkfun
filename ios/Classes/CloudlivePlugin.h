90ae19d5bd723d587c82e217aad772b6094b883d
liliang <1582880360@qq.com>
2019-02-28 23:43:06 +0800

    1.初始化。

---
+++ ios/Classes/CloudlivePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface CloudlivePlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/CloudlivePlugin.m
@@ -0,0 +1,20 @@
+#import "CloudlivePlugin.h"
+
+@implementation CloudlivePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"cloudlive"
+            binaryMessenger:[registrar messenger]];
+  CloudlivePlugin* instance = [[CloudlivePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
